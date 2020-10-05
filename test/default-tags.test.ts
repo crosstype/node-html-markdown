9445ee3ff4fcc2d3213453587a28f5a5476745e8
Ron S <ron@nonara.com>
2020-10-05 19:45:25 -0400

    Added default tags tests

---
+++ test/default-tags.test.ts
@@ -0,0 +1,129 @@
+import { NodeHtmlMarkdown } from '../src';
+
+
+/* ****************************************************************************************************************** *
+ * Tests
+ * ****************************************************************************************************************** */
+
+describe(`Default Tags`, () => {
+  let instance: NodeHtmlMarkdown;
+  const translate = (html: string) => instance.translate(html);
+  beforeAll(() => {
+    instance = new NodeHtmlMarkdown();
+  });
+
+  test(`Line Break (br)`, () => {
+    const res = translate(`a<br>b`);
+    expect(res).toBe(`a  \nb`);
+  });
+
+  test(`Horizontal Rule (hr)`, () => {
+    const res = translate(`a<hr>b`);
+    expect(res).toBe(`a\n\n---\n\nb`);
+  });
+
+  test(`Bold (b, strong)`, () => {
+    const res = translate(`<b>a<del>b</del><br><br>c<br>d</b><strong>a<del>b</del><br><br>c<br>d</strong>`);
+    const exp = `**a~~b~~**\n  \n**c**\n**d**`;
+    expect(res).toBe(exp + ' ' + exp);
+  });
+
+  test(`Strikethrough (del, s, strike)`, () => {
+    const res = translate(`<del>a<em>b</em><br><br>c<br>d</del><s>a<em>b</em><br><br>c<br>d</s><strike>a<em>b</em><br><br>c<br>d</strike>`);
+    expect(res).toBe(`~~a_b_~~\n  \n~~c~~\n~~d~~ `.repeat(3).trim());
+  });
+
+  test(`Italic / Emphasis (em, i)`, () => {
+    const res = translate(`<em>a<s>b</s><br><br>c<br>d</em><i>a<s>b</s><br><br>c<br>d</i>`);
+    expect(res).toBe(`_a~~b~~_\n  \n_c_\n_d_ `.repeat(2).trim());
+  });
+
+  test(`Link (a)`, () => {
+    const url = 'http://www.github.com/crosstype';
+    const res = translate(`
+        <a href="${url}">a<br><br>b<strong>c</strong></a>
+        <a>a<strong>b</strong></a> <!-- This node is treated as text due to no href -->
+    `);
+    expect(res).toBe(`[a    b**c**](${url})a**b**`);
+  });
+
+  test(`Image (img)`, () => {
+    const url = `http://www.github.com/crosstype/`
+    const res = translate(`
+        <img src="${url}1">
+        <img title="t2" alt="a2"> <!-- This node is elided due to no href -->
+        <img src="${url}3" title="t3">
+        <img src="${url}4" title="t4" alt="a4">
+    `);
+    expect(res).toBe(`![](${url}1)` + `![](${url}3 "t3")` + `![a4](${url}4 "t4")`);
+  });
+
+  test(`Pre-formatted Text (pre)`, () => {
+    const str = `* test\n1. test\n\\Test`;
+    const res = translate(`<pre>${str}<br><b># hello</b></pre>`);
+    expect(res).toBe(str + '  \n**# hello**');
+  });
+
+  test(`Block Quote (blockquote)`, () => {
+    const res = translate(`<blockquote>a<br>b<br>c<blockquote>def</blockquote>`);
+    expect(res).toBe(`> a  \n> b  \n> c\n> \n>> def`);
+  });
+
+  test(`Headings (h1, h2, h3, h4, h5, h6)`, () => {
+    const res = translate(
+      `<h1>a<b>b</b></h1><h2>a<b>b</b></h2><h3>a<b>b</b></h3><h4>a<b>b</b></h4><h5>a<b>b</b></h5><h6>a<b>b</b></h6>`
+    );
+    expect(res).toBe(Array.from(Array(6), (v, i) => `#`.repeat(i+1) + ` a**b**\n\n`).join('').trim());
+  });
+
+  test(`Code (code)`, () => {
+    const res = translate('<code>```` a    \n\nb\n* c</code><code>d</code>');
+    expect(res).toBe('````` ```` a    \nb\n* c ````` `d`');
+  });
+
+  describe(`Code-block (pre + code)`, () => {
+    const str = `* test  \n\n1. test\n\\Test`;
+
+    test(`Fenced`, () => {
+      const res = translate(`<pre><code class="language-fortran">${str}</code></pre><pre><code>${str}</code></pre>`);
+      expect(res).toBe('```fortran\n'+str+'\n```\n\n```\n'+str+'\n```');
+    });
+
+    test(`Indented`, () => {
+      const originalCodeFence = instance.options.codeBlockStyle;
+      instance.options.codeBlockStyle = 'indented';
+
+      const res = translate(`<pre><code class="language-fortran">${str}</code></pre><pre><code>${str}</code></pre>`);
+      const exp = str.replace(/^/gm, '    ');
+      expect(res).toBe(exp + '\n\n' + exp);
+
+      instance.options.codeFence = originalCodeFence;
+    });
+  });
+
+  test(`Lists (ol + li, ul + li)`, () => {
+    const olFirst = translate(`
+        <ol>
+          <li>a<br><br><s>b</s></li>
+          <li> </li> <!-- Elided due to whitespace -->
+          <li>b
+            <ol><li>c<br>d</li></ol>
+            <ul><li>e<br>f</li></ul>
+          </li>
+        </ol>
+    `);
+    expect(olFirst).toBe(`1. a  \n    \n~~b~~\n2. b  \n   1. c  \n   d  \n   * e  \n   f  `);
+
+    const ulFirst = translate(`
+        <ul>
+          <li>a<br><br><s>b</s></li>
+          <li> </li> <!-- Elided due to whitespace -->
+          <li>b
+            <ul><li>c<br>d</li></ul>
+            <ol><li>e<br>f</li></ol>
+          </li>
+        </ul>
+    `);
+    expect(ulFirst).toBe(`* a  \n    \n~~b~~\n* b  \n   * c  \n   d  \n   1. e  \n   f  `);
+  });
+});
