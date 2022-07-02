4f541013e61d4ba3fec25097a3a9a19bfe6d708c
SettingDust <settingdust@gmail.com>
2022-07-02 08:52:57 +0800

    fix: remove `perfStart` and `perfStop`

--- transformer.js
+++ transformer.js
@@ -3,6 +3,12 @@ module.exports = function (program, cfg, { ts }) {
   return (ctx) => {
     return function visit(node) {
       if (process.env.CI || !cfg.removePerf) return node;
+      // Remove the functions
+      if (ts.isFunctionDeclaration(node)
+        && [ 'perfStart', 'perfStop' ].includes(node.name.escapedText))
+        return undefined
+
+      // Remove the invokes
       if (
         ts.isExpressionStatement(node) && ts.isCallExpression(node.expression) &&
         [ 'perfStart', 'perfStop' ].includes(node.expression.expression.text)
