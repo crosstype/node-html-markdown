c7fd8ff8c596ab9cb5519fe8af79c5bf53c6efb9
SettingDust <settingdust@gmail.com>
2022-12-06 19:58:24 +0800

    test: add test for `TranslatorCollection`

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -2,13 +2,15 @@ import { NodeHtmlMarkdown } from '../src';
 import { DOMParser } from 'linkedom';
 
 
-const html = `<html><body><p><b>F</b><span>oo</span></p><ul><li>Bar</li></ul></body></html>`
+const html = `<html><body><p><b>F</b><span>oo</span></p><ul><li>Bar</li></ul></body></html>`;
 
-test('Browser', () => {
-  __IS_BROWSER__ = true
-  globalThis.DOMParser = <typeof globalThis.DOMParser>DOMParser
-  const res = new NodeHtmlMarkdown({
-    preferNativeParser: true
-  }).translate(html);
-  expect(res).toBe(`**F**oo\n\n* Bar`);
-})
+describe('E2E', () => {
+  test('Browser', () => {
+    __IS_BROWSER__ = true;
+    globalThis.DOMParser = <typeof globalThis.DOMParser>DOMParser;
+    const res = new NodeHtmlMarkdown({
+      preferNativeParser: true
+    }).translate(html);
+    expect(res).toBe(`**F**oo\n\n* Bar`);
+  });
+});
